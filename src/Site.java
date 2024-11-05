d671c944782cce4d5fa11f743d1189061cc256af
iheanyi <iheanyinjokuobi@gmail.com>
2024-11-05 15:19:34 -0500

    I have made the getBaseAmount and getTaxAmount abstract methods so each child class can implement them individually. I also made the getBillableAmount method just call those 2 methods.

--- src/Site.java
+++ src/Site.java
@@ -1,4 +1,12 @@
 public abstract class Site {
     protected static int TAX_RATE = 10;
 
+    public double getBillableAmount(){
+        return getBaseAmount() + getTaxAmount();
+    }
+
+    public abstract double getBaseAmount();
+
+    public abstract double getTaxAmount();
+
 }
